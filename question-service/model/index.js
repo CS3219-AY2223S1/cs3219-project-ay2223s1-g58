08ac070e214a32f52855b615d2a109e65d038bed
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-09-08 03:48:19 +0800

    Dockerize question-service

--- question-service/model/index.js
+++ question-service/model/index.js
@@ -4,17 +4,26 @@ require("dotenv").config();
 
 // pg-promise initialization options:
 const initOptions = {
-    promiseLib: promise
+    promiseLib: promise,
+    error: function (error, e) {
+        if (e.cn) {
+            console.log("Error: " + error)
+            console.log("CONNECTION:", e.cn);
+        }
+    }
 };
 
 
 const pgp = pgPromise(initOptions)
+
 const db = pgp({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT
 });
+
+
 
 module.exports =  db ;
