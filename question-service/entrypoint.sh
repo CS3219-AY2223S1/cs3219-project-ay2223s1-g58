de31077705a3d9e800ff55bd0558fe12f16034d7
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-09-15 19:40:28 +0800

    Fix docker compose issues

--- question-service/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e           # Stop on any error
-npx sequelize-cli db:migrate --env production
-npx sequelize-cli db:seed:all --env production 
-exec "$@"        # Run the command as the main container process
