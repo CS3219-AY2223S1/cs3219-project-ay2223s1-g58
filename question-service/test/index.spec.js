305722429b9af7be8361fd66461c743ca87ecf7a
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-09-16 00:21:25 +0800

    Update index.spec

--- question-service/test/index.spec.js
+++ question-service/test/index.spec.js
@@ -1,5 +1,5 @@
-import request from 'supertest'
-import { app } from '../index.js'
+const request = require('supertest')
+const app = require('../index.js')
 
 describe('GET /status', function () {
     it('responds successfully', function (done) {
