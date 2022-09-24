e9a80e37b13571b1f5e808e0822e760f80d5a5c2
uyencfi <77076051+uyencfi@users.noreply.github.com>
2022-09-24 10:22:32 +0800

    Detech invalid room URL. Add Helmet

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -1,8 +1,9 @@
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from '../api/axios'
 import { useEffect, useState } from 'react'
-import { URL_MATCHING_ROOM, STATUS_CODE_SUCCESS } from '../constants'
+import { URL_MATCHING_ROOM, STATUS_CODE_SUCCESS, STATUS_CODE_BAD_REQUEST } from '../constants'
 import { useToast } from '@chakra-ui/react'
+import { Helmet } from 'react-helmet-async'
 import QuestionPane from '../components/QuestionPane'
 import Editor from '../components/collaboration/Editor'
 import { Button } from '../components/Button'
@@ -11,6 +12,7 @@ const Room = () => {
   const navigate = useNavigate()
   const { roomId } = useParams()
   const [questionId, setQuestionId] = useState()
+  const [isValid, setIsValid] = useState(true)
   const toast = useToast()
 
   useEffect(() => {
@@ -19,7 +21,9 @@ const Room = () => {
 
   const getQuestionId = async () => {
     const res = await axios.get(`${URL_MATCHING_ROOM}/${roomId}`).catch((e) => {
-      console.log(e)
+      if (e.response.status === STATUS_CODE_BAD_REQUEST) { // Room not found
+        return setIsValid(false)
+      }
     })
     if (res && res.status === STATUS_CODE_SUCCESS) {
       setQuestionId(res.data.data.questionId)
@@ -29,7 +33,7 @@ const Room = () => {
   const endSession = async () => {
     toast({
       title: 'Session ended!',
-      description: "Going to Home...",
+      description: 'Going to Home...',
       status: 'success',
       duration: 4000,
       isClosable: true,
@@ -40,12 +44,41 @@ const Room = () => {
     }, 4000)
   }
 
+  const getHelmet = () => {
+    return (
+      <Helmet>
+        <title>Room | PeerPrep</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="PeerPrep help you prep" />
+      </Helmet>
+    )
+  }
+
+  if (!isValid) {
+    return (
+      <>
+        {getHelmet()}
+        <main className="flex h-full flex-col items-center justify-start">
+          <h1>Invalid Room... The session has ended.</h1>
+        </main>
+      </>
+    )
+  }
+
   if (!questionId) {
-    return <h2>Retrieving room...</h2>
+    return (
+      <>
+        {getHelmet()}
+        <main className="flex h-full flex-col items-center justify-start">
+          <h1>Retrieving room...</h1>
+        </main>
+      </>
+    )
   }
 
   return (
     <>
+      {getHelmet()}
       <div className="grid h-screen grid-cols-2 gap-4">
         <QuestionPane id={questionId} />
 
