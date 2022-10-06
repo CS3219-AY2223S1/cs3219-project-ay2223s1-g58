a41c6cb55fc0e925367f89ba1c32c939ce92aad3
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-10-06 22:16:02 +0800

    Reformat code

--- frontend/src/components/QuestionPane.jsx
+++ frontend/src/components/QuestionPane.jsx
@@ -1,6 +1,14 @@
-import { Text, Box, Badge, HStack, VStack, Heading, Divider } from '@chakra-ui/react'
+import {
+  Text,
+  Box,
+  Badge,
+  HStack,
+  VStack,
+  Heading,
+  Divider,
+} from '@chakra-ui/react'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
-import { AuthLayout } from '../components/AuthLayout';
+import { AuthLayout } from '../components/AuthLayout'
 import ReactMarkdown from 'react-markdown'
 import { Button } from '../components/Button'
 import { URL_QUESTION_SERVICE } from '../constants'
@@ -29,70 +37,88 @@ const parse = (text) => {
   return text
 }
 
-
-const QuestionPane = ({id}) => {
+const QuestionPane = ({ id }) => {
   const [questionId, setQuestionId] = useState(id)
   const [questionData, setQuestionData] = useState([])
 
   useEffect(() => {
     const getQuestion = async () => {
-      await axios.get(URL_QUESTION_SERVICE + '?id=' + questionId)
-      .then((response) => {
-        var newData = response.data
-        setQuestionData(newData)
-      }).catch((e) => console.log(e))
+      await axios
+        .get(URL_QUESTION_SERVICE + '?id=' + questionId)
+        .then((response) => {
+          var newData = response.data
+          setQuestionData(newData)
+        })
+        .catch((e) => console.log(e))
     }
     getQuestion()
   }, [questionId, questionData])
 
   const getNextQuestion = async () => {
-    const {data: response} = await axios.get(URL_QUESTION_SERVICE + '?id=' + questionId).catch((e) => console.log(e))
-    await axios.get(URL_QUESTION_SERVICE + '/nextQuestion' 
-    + '?difficulty=' + response.difficulty  + '&past_id=' + questionData.id).then((response) => {
-      var newData = response.data
-      setQuestionId(newData.id)
-    }).catch(console.log)
+    const { data: response } = await axios
+      .get(URL_QUESTION_SERVICE + '?id=' + questionId)
+      .catch((e) => console.log(e))
+    await axios
+      .get(
+        URL_QUESTION_SERVICE +
+          '/nextQuestion' +
+          '?difficulty=' +
+          response.difficulty +
+          '&past_id=' +
+          questionData.id
+      )
+      .then((response) => {
+        var newData = response.data
+        setQuestionId(newData.id)
+      })
+      .catch(console.log)
   }
 
   if (typeof questionData === 'undefined' || questionData.length === 0) {
     return (
       <AuthLayout title="Retrieving question...">
-            <div className="text-xl text-center">
-            </div>
-          </AuthLayout>
+        <div className="text-center text-xl"></div>
+      </AuthLayout>
     )
   }
 
   const difficultyColor = (difficulty) => difficultyColorMap.get(difficulty)
 
-
   return (
-      <>
-          {
-          (
-              <Box className='rounded-lg border'>
-                  <VStack h='100vh'>
-                      <HStack spacing='30px'>
-                          <Heading size='lg' fontWeight='semibold' color='gray 500'>
-                            {questionData.name}
-                          </Heading> 
-                          <Badge borderRadius='full' px='2' colorScheme={difficultyColor(questionData.difficulty)}>
-                              {questionData.difficulty}
-                          </Badge>
-                          <Button onClick={getNextQuestion} variant="outline">Next Question</Button>
-                      </HStack>
+    <>
+      {
+        <Box className="rounded-lg border">
+          <VStack h="100vh">
+            <HStack spacing="30px">
+              <Heading size="lg" fontWeight="semibold" color="gray 500">
+                {questionData.name}
+              </Heading>
+              <Badge
+                borderRadius="full"
+                px="2"
+                colorScheme={difficultyColor(questionData.difficulty)}
+              >
+                {questionData.difficulty}
+              </Badge>
+              <Button onClick={getNextQuestion} variant="outline">
+                Next Question
+              </Button>
+            </HStack>
 
-                      <Divider orientation='horizontal' />
+            <Divider orientation="horizontal" />
 
-                      <div className="mx-2 px-2 max-h-full overflow-y-auto">
-                          <ReactMarkdown components={ChakraUIRenderer(newTheme)} children={parse(questionData.content)} skipHtml/>;
-                      </div>
-                  </VStack>
-              </Box>
-          )}
-      </>
-
-
+            <div className="mx-2 max-h-full overflow-y-auto px-2">
+              <ReactMarkdown
+                components={ChakraUIRenderer(newTheme)}
+                children={parse(questionData.content)}
+                skipHtml
+              />
+              ;
+            </div>
+          </VStack>
+        </Box>
+      }
+    </>
   )
 }
 
