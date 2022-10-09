cabaa84a7af65d0dbfd6965748b021ff0fc0e19e
Hzxin <61725744+Hzxin@users.noreply.github.com>
2022-10-09 19:36:22 +0800

    Question service api (#103)
    * Add new apis and unit test
    
    * Add nextQuestionButton & windows CI
    
    * Update github workflow
    
    * update test
    
    * Update workflow
    
    * Remove windows test
    
    * Update QuestionPane
    
    * Update host
    
    * Modify Questionpane design
    
    * Remove extra parameter for question pane
    
    * Fix eslint error
    
    * Fix bug
    
    * Refactor code foramt
    
    * Update .github/workflows/question-service.yml
    
    Co-authored-by: Liu YongLiang <41845017+tlylt@users.noreply.github.com>
    
    * Update frontend/src/components/QuestionPane.jsx
    
    Co-authored-by: Liu YongLiang <41845017+tlylt@users.noreply.github.com>
    
    * Update question-service/repository/category-repository.js
    
    Co-authored-by: Liu YongLiang <41845017+tlylt@users.noreply.github.com>
    
    * Update question-service/repository/category-repository.js
    
    Co-authored-by: Liu YongLiang <41845017+tlylt@users.noreply.github.com>
    
    * Update frontend/src/components/QuestionPane.jsx
    
    Co-authored-by: Liu YongLiang <41845017+tlylt@users.noreply.github.com>
    
    * Refactor update controller
    
    * Format fix
    
    Co-authored-by: Liu YongLiang <41845017+tlylt@users.noreply.github.com>

--- frontend/src/components/QuestionPane.jsx
+++ frontend/src/components/QuestionPane.jsx
@@ -7,9 +7,13 @@ import {
   Heading,
   Divider,
 } from '@chakra-ui/react'
-import useFetchQuestionById from '../hooks/useFetchQuestionById'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
+import { AuthLayout } from '../components/AuthLayout'
 import ReactMarkdown from 'react-markdown'
+import { Button } from '../components/Button'
+import { URL_QUESTION_SERVICE } from '../constants'
+import axios from '../api/axios'
+import { useEffect, useState } from 'react'
 
 const difficultyColorMap = new Map([
   ['easy', 'green'],
@@ -34,7 +38,49 @@ const parse = (text) => {
 }
 
 const QuestionPane = ({ id }) => {
-  const { data, loading } = useFetchQuestionById(id)
+  const [questionId, setQuestionId] = useState(id)
+  const [questionData, setQuestionData] = useState([])
+
+  useEffect(() => {
+    const getQuestion = async () => {
+      await axios
+        .get(URL_QUESTION_SERVICE + '?id=' + questionId)
+        .then((response) => {
+          const newData = response.data
+          setQuestionData(newData)
+        })
+        .catch((e) => console.log(e))
+    }
+    getQuestion()
+  }, [questionId, questionData])
+
+  const getNextQuestion = async () => {
+    const { data: response } = await axios
+      .get(URL_QUESTION_SERVICE + '?id=' + questionId)
+      .catch((e) => console.log(e))
+    await axios
+      .get(
+        URL_QUESTION_SERVICE +
+          '/nextQuestion' +
+          '?difficulty=' +
+          response.difficulty +
+          '&past_id=' +
+          questionData.id
+      )
+      .then((response) => {
+        const newData = response.data
+        setQuestionId(newData.id)
+      })
+      .catch(console.log)
+  }
+
+  if (typeof questionData === 'undefined' || questionData.length === 0) {
+    return (
+      <AuthLayout title="Retrieving question...">
+        <div className="text-center text-xl"></div>
+      </AuthLayout>
+    )
+  }
 
   const difficultyColor = (difficulty) => difficultyColorMap.get(difficulty)
 
@@ -42,34 +88,31 @@ const QuestionPane = ({ id }) => {
     <>
       <Box className="rounded-lg border">
         <VStack h="100vh">
-          {loading || !data ? (
-            <div className="text-center text-xl">Retrieving Question</div>
-          ) : (
-            <>
-              <HStack spacing="24px">
-                <Heading size="lg" fontWeight="semibold" color="gray 500">
-                  {data.name}
-                </Heading>
-                <Badge
-                  borderRadius="full"
-                  px="2"
-                  colorScheme={difficultyColor(data.difficulty)}
-                >
-                  {data.difficulty}
-                </Badge>
-              </HStack>
+          <HStack spacing="30px">
+            <Heading size="lg" fontWeight="semibold" color="gray 500">
+              {questionData.name}
+            </Heading>
+            <Badge
+              borderRadius="full"
+              px="2"
+              colorScheme={difficultyColor(questionData.difficulty)}
+            >
+              {questionData.difficulty}
+            </Badge>
+            <Button onClick={getNextQuestion} variant="outline">
+              Next Question
+            </Button>
+          </HStack>
 
-              <Divider orientation="horizontal" />
+          <Divider orientation="horizontal" />
 
-              <div className="mx-2 max-h-full overflow-y-auto px-2">
-                <ReactMarkdown
-                  components={ChakraUIRenderer(newTheme)}
-                  children={parse(data.content)}
-                  skipHtml
-                />
-              </div>
-            </>
-          )}
+          <div className="mx-2 max-h-full overflow-y-auto px-2">
+            <ReactMarkdown
+              components={ChakraUIRenderer(newTheme)}
+              children={parse(questionData.content)}
+              skipHtml
+            />
+          </div>
         </VStack>
       </Box>
     </>
