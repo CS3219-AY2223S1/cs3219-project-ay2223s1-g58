22bbd0b8aea65fb48a23d5d4ccb14b11c4f6c43b
tlylt <liu_yongliang@hotmail.com>
2022-10-19 23:41:18 +0800

    Update domain details

--- k8s/init.sh
+++ k8s/init.sh
@@ -19,6 +19,20 @@ chmod 700 get_helm.sh
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm install my-release ingress-nginx/ingress-nginx
 
+## Install https cert manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.8.0 \
+  --set installCRDs=true
+
+## get certificates
+kubectl get certificates
+kubectl describe certificates
+
 ################################################################################################################
 ################################################################################################################
 
