88c5164e8954a434dfe5f6ef887890778a19576d
uyencfi <77076051+uyencfi@users.noreply.github.com>
2022-10-20 00:11:56 +0800

    Update services in docker

--- matching-service/const/constants.js
+++ matching-service/const/constants.js
@@ -16,16 +16,6 @@ exports.DIFFICULTY = {
   HARD: "hard",
 };
 
-const questionServiceFallback =
-  process.env.ENV === "production" // production means using Docker
-    ? `http://${process.env.QUESTION_SERVICE_HOST}:8500`
-    : "http://localhost:8500";
-const URI_QUESTION_SERVICE =
-  process.env.URI_QUESTION_SERVICE || questionServiceFallback;
-const PREFIX_QUESTION_SERVICE = "/api/v1/question";
-
-exports.URL_QUESTION_SERVICE = URI_QUESTION_SERVICE + PREFIX_QUESTION_SERVICE;
-
 const roomServiceFallback =
   process.env.ENV === "production" // production means using Docker
     ? `http://${process.env.ROOM_SERVICE_HOST}:8022`
