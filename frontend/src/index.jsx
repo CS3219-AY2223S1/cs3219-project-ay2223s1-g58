13a9a51905000dee5367784b2732ca5cde169bef
Marcus Tang <txk.marcus@gmail.com>
2022-10-19 22:15:37 +0800

    Re-enable react strict mode

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -12,16 +12,16 @@ import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  // <React.StrictMode>
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <HelmetProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HelmetProvider>
-  </ChakraProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <HelmetProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </HelmetProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
